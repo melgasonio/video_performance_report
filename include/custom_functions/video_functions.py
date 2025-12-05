208165e0102daa54aafa1b65f33f7d864050e5e1
unknown <soniomelga@gmail.com>
2025-12-05 04:08:58 -0500

    feat: start working on the DAG file

---
+++ include/custom_functions/video_functions.py
@@ -0,0 +1,6 @@
+# Returns the a dict object defining the stats of the two videos: 
+def get_videos_stats():
+    video_ids = ["BpEdIm-IBCk", "a-jhSWSjoMA"]
+    
+    # TODO: Update this with the desired functionality. This is a placeholder only.
+    print(video_ids)
