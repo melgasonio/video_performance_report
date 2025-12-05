090f7d07b982bd42bc079047c32ee66eaedfff45
unknown <soniomelga@gmail.com>
2025-12-05 04:20:24 -0500

    feat: add dag parameters

--- dags/video_data_etl.py
+++ dags/video_data_etl.py
@@ -17,3 +17,19 @@ import os
 # Modularized helper functions
 from include.custom_functions.video_functions import get_videos_stats
 
+@dag(
+    start_date=datetime(2025, 12, 8),
+    schedule="@daily",
+    max_consecutive_failed_dag_runs=5,
+    max_active_runs=1,
+    doc_md = __doc__,
+    default_args={
+        "owner": "Melga Sonio",
+        "retries": 3,
+        "retry_delay": duration(seconds=30),
+    },
+    tags=["data project", "ETL"],
+    is_paused_upon_creation=True,
+)
+def video_data_etl():
+    
