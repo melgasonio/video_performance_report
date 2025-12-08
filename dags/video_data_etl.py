5dd1e28d2f828e52f0461d3fa9b1cb5314c79eab
unknown <soniomelga@gmail.com>
2025-12-08 02:57:48 -0500

    feat: complete DAG tasks and add dependencies chain

--- dags/video_data_etl.py
+++ dags/video_data_etl.py
@@ -15,12 +15,18 @@ import logging
 import os
 
 # Modularized helper functions
-from include.custom_functions.video_functions import get_videos_stats
+from include.custom_functions.video_functions import get_videos_stats, transform_df
 
+# Use the Airflow task logger to log information to the task logs
 t_log = logging.getLogger("airflow.task")
 
+# Define variables used in a DAG as environment variables in .env for your Airflow instance to standardize your DAGs
+_DUCKDB_INSTANCE_NAME = os.getenv("DUCK_DB_INSTANCE_NAME", "include/performance.db")
+_DUCKDB_TABLE_NAME = os.getenv("DUCKDB_TABLE_NAME", "performance_data")
+_DUCKDB_TABLE_URI = f"duckdb://{_DUCKDB_INSTANCE_NAME}/{_DUCKDB_TABLE_NAME}"
+
 @dag(
-    start_date=datetime(2025, 12, 8),
+    start_date=datetime(2025, 12, 10),
     schedule="@daily",
     max_consecutive_failed_dag_runs=5,
     max_active_runs=1,
@@ -48,12 +54,12 @@ def video_data_etl():
         cursor.execute(
             f"""
             CREATE TABLE IF NOT EXISTS {table_name} (
-                id INT PRIMARY KEY,
-                name STRING,
+                id VARCHAR PRIMARY KEY,
+                name VARCHAR,
                 view INT,
                 comments INT,
-                likes INT,
-            )"""
+                likes INT
+            );"""
         )
         cursor.close()
         
@@ -61,39 +67,60 @@ def video_data_etl():
         
     # Extract task
     @task
-    def extract_video_data(num_videos: int = _NUM_VIDEOS_TOTAL) -> pd. DataFrame:
-        videos_df = get_videos_stats(num_videos)
+    def extract_video_data() -> pd.DataFrame:
+        videos_df = get_videos_stats()
         
         return videos_df
     
     # Transform task
     @task
-    def transform_video_data(
-        videos_df: pd.DataFrame, 
-        stats_list: list = _STATS_LIST
-    ) -> pd.DataFrame:
-        tranformed_df = transform_by_stats(stats_list)
+    def transform_video_data(videos_df: pd.DataFrame) -> pd.DataFrame:
+        transformed_df = transform_df(videos_df)
         
         t_log.info("Transforming dataset based on specific stats needed")
         
-        return tranformed_df
+        return transformed_df
     
     # Load task
-    @task
+    @task(
+        outlets=[Asset(_DUCKDB_TABLE_URI)]
+    ) # Define that this task produces updates to an Airflow dataset
     def load_video_data(
         transformed_df: pd.DataFrame,
-        duckdb_instance_name: str = _DUCK_DB_INSTANCE_NAME,
+        duckdb_instance_name: str = _DUCKDB_INSTANCE_NAME,
         table_name: str = _DUCKDB_TABLE_NAME
     ):
         t_log.info("Loading video data into DuckDB")
         cursor = duckdb.connect(duckdb_instance_name)
+        cursor.register("df", transformed_df)
         cursor.sql(
-            f"INSERT OR IGNORE INTO {table_name} BY ID SELECT * FROM transformed_df"
+            f"INSERT OR REPLACE INTO {table_name} SELECT * FROM df;"
         )
         
         t_log.info("Videos data loaded into DuckDB")
         
+    # Prints loaded data to DuckDB
+    @task
+    def print_loaded_data(
+        duckdb_instance_name: str = _DUCKDB_INSTANCE_NAME,
+        table_name: str = _DUCKDB_TABLE_NAME,
+    ):
+        cursor = duckdb.connect(duckdb_instance_name)
+        loaded_data_df = cursor.sql(f"SELECT * FROM {table_name};").df()
+
+        t_log.info(tabulate(loaded_data_df, headers="keys", tablefmt="pretty"))
+        
+        
+    create = create_perf_table_in_duckdb()
+    extract = extract_video_data()
+    transform = transform_video_data(extract)
+    load = load_video_data(transform)
+    printer = print_loaded_data()
     
+    create >> extract >> transform >> load >> printer
+
+# Instantiate the DAG
+video_data_etl()
     
     
     
